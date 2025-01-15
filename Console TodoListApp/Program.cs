3d063cbae34ad2373f9ab9759e5964654a0cfa1b
nyinyiminthant-dev <nnyi37389@gmail.com>
2025-01-15 14:59:09 +0630

    {Complete} Task

--- Console TodoListApp/Program.cs
+++ Console TodoListApp/Program.cs
@@ -5,50 +5,55 @@ using System.Security.Cryptography;
 using Console_TodoListApp.EfcoreExamples;
 
 Console.WriteLine(value: "Welcome To Console Todo-list App");
-
 Console.WriteLine();
+
 EfcoreExample efcore = new EfcoreExample();
 
-Console.Write("Register (r), ");
+int count = 1;
+int lcount = 1;
 
-
-Console.Write("Login (l), ");
-Console.WriteLine();
-
-
-Console.WriteLine("____________________________________________");
-
-
-string start = Console.ReadLine()!;
-
-
-
-#region Registration 
-
-if (start is "r" || start is "R")
+while (count is 1 ||count > 1)
 {
-    Console.WriteLine("Registration Account.");
-    Console.WriteLine("__________________________");
-    Console.WriteLine();
-    Console.WriteLine("Please Set Your Email");
-    Console.WriteLine();
-    string email = Console.ReadLine()!; 
+    #region Whole App
+
+    Console.Write("Register (r), ");
+
+
+    Console.Write("Login (l), ");
     Console.WriteLine();
 
-    Console.WriteLine("Please Set Your Password");
-    Console.WriteLine();
-    string password = Console.ReadLine()!;
-    Console.WriteLine();
 
-    efcore.CreateUser(email,password);
-    Console.WriteLine("____________________");
-    Console.WriteLine();
+    Console.WriteLine("____________________________________________");
 
-}
 
-#endregion
+    string start = Console.ReadLine()!;
 
-#region Login
+
+
+    #region Registration 
+
+    if (start is "r" || start is "R")
+    {
+        Console.WriteLine("Registration Account.");
+        Console.WriteLine("__________________________");
+        Console.WriteLine();
+        Console.WriteLine("Please Set Your Email");
+        Console.WriteLine();
+        string email = Console.ReadLine()!;
+        Console.WriteLine();
+
+        Console.WriteLine("Please Set Your Password");
+        Console.WriteLine();
+        string password = Console.ReadLine()!;
+        Console.WriteLine();
+
+        efcore.CreateUser(email, password);
+
+    }
+
+    #endregion
+
+    #region Login
 
 
 
@@ -70,36 +75,76 @@ if (start is "r" || start is "R")
 
         efcore.LoginUser(email, password);
 
-        
+
        
-        
 
-        Console.WriteLine("Add the Task   write : (nt) || Done    write : (dt) || exit write : (e)" );
-        
-       string task = Console.ReadLine()!;   
-       Console.WriteLine();
+        while (lcount is 1 || lcount > 1)
+        {
 
-       if(task is "nt")
+            Console.WriteLine("Add the Task : (nt) || Done : (dt) || exit : (e)");
+
+            string task = Console.ReadLine()!;
+            Console.WriteLine();
+
+            if (task is "nt")
+            {
+                Console.WriteLine("Please enter your task");
+                string title = Console.ReadLine()!;
+                Console.WriteLine();
+                efcore.WriteTask(email, title);
+                lcount++;
+                
+            }
+            if (task is "dt")
+            {
+                Console.WriteLine("Please enter the title that you have done.");
+                string title = Console.ReadLine()!;
+                Console.WriteLine();
+                efcore.DeleteTask(email, title);
+                lcount++;
+               
+            }
+            if (task is "e")
+            {
+                Console.WriteLine("_____________________");
+                Console.WriteLine();
+                lcount = 0;
+               
+            }
+            else
+            {
+                Console.WriteLine();
+                lcount++;
+            }
+            
+           
+        }
+
+
+
+
+    }
+
+    Console.WriteLine("Do you want to start again? (y) or (n)");
+    string round = Console.ReadLine()!;
+    Console.WriteLine();
+
+    if (round is "Y" || round is "y")
     {
-        Console.WriteLine("Please enter your task");
-        string title = Console.ReadLine()!;
+        count++;
+        Console.WriteLine("Welcome To Console Todo-list App again.");
         Console.WriteLine();
-        efcore.WriteTask(email,title);
-    } 
-       if(task is "dt")
-    {
-        Console.WriteLine("Please enter the title that you have done.");
-        string title = Console.ReadLine()!;
-        Console.WriteLine();
-        efcore.DeleteTask(email, title);
-    }  if(task is "e")
+    }
+    else if (round is "N" || round is "n")
     {
         Console.WriteLine("Thank For Using");
-      
-        
+        count = 0;
     }
-    
+    #endregion
+
+    #endregion
+
 }
-#endregion
+
 
 Console.ReadLine();
